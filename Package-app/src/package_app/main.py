c4b383f01c62bae430bc673db68a28e1a1c9b2a9
=Mujahid Iqbal <=mujahidsir@gmail.com>
2025-08-29 22:44:08 +0500

    first commit

---
+++ Package-app/src/package_app/main.py
@@ -0,0 +1,12 @@
+def hello():
+    print("\nHello from package-app!\n")
+    print("Mujahid Iqbal'")
+    print("PIAIC PIAIC62907\n")
+
+def add_numbers():
+    a = input("Enter first number: ")
+    b = input("Enter second number: \n")
+    a = int(a)
+    b = int(b)
+    print("Sum of a and b is:", a + b)  
+
