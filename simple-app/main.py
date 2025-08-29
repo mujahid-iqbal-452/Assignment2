950314ea7f22b40b26bbf328a7d327a1d761a915
=Mujahid Iqbal <=mujahidsir@gmail.com>
2025-08-29 23:12:03 +0500

    second commit

---
+++ simple-app/main.py
@@ -0,0 +1,9 @@
+print("Hello from simple-app!\n")
+print("Mujahid Iqbal")
+print("PIAIC PIAIC62907\n")
+
+a = input("Enter first number: ")
+b = input("Enter second number: \n")
+a = int(a)
+b = int(b)
+print("sum of a  and b  " , a +  b)
